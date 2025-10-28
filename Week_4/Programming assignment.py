30644143da818baddf7bc8881ff291adb049a3af
zexal20951675 <140132945+zexal20951675@users.noreply.github.com>
2025-10-28 23:31:06 +0800

    Update Programming assignment.py

--- Week_4/Programming assignment.py
+++ Week_4/Programming assignment.py
@@ -1,3 +1,7 @@
+# Week 4 Programming Assignment
+# Author: 陳伯杰(Using ChatGPT)
+# Date: 2025
+
 from google.colab import files
 uploaded = files.upload()   # 會跳出上傳視窗，選擇 O-A0038-003.xml
 print("已上傳檔案：", list(uploaded.keys()))
